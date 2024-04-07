ca186947a1240564a1e0de76f0d4f510b17054e6
Susanto <sushantomahato1@gmail.com>
2024-04-07 11:36:39 +0530

    feat: authentication

--- src/App.js
+++ src/App.js
@@ -1,22 +1,35 @@
 // import Counter from "./components/Counter";
 import { useSelector, useDispatch } from "react-redux";
+import { authActions, counterActions } from "./store";
 
 function App() {
   const dispatch = useDispatch();
-  const counter = useSelector((store) => store.counter);
-  const show = useSelector((store) => store.showCounter);
+  const counter = useSelector((store) => store.counter.counter);
+  const show = useSelector((store) => store.counter.showCounter);
+  const auth = useSelector((store) => store.auth.isAuthenticated);
   const incrementBy5 = () => {
-    dispatch({ type: "incrementBy5" });
+    dispatch(counterActions.increase(5));
   };
   const decrementBy5 = () => {
-    dispatch({ type: "decrementBy5" });
+    dispatch(counterActions.decrement());
   };
   const toggleCounter = () => {
-    dispatch({ type: "toggle" });
+    dispatch(counterActions.toggleCounter());
+  };
+
+  const handleAuth = () => {
+    dispatch(authActions.login());
+  };
+  const handleAuthlogout = () => {
+    dispatch(authActions.logout());
   };
   return (
     <>
       {/* <Counter /> */}
+      {!auth && <p>Not Authenticated</p>}
+      {auth && <p>Is Authenticated</p>}
+      <button onClick={handleAuth}>Authenticate</button>
+      <button onClick={handleAuthlogout}>Logout</button>
       {show && (
         <div>
           <p>{counter}</p>
--- src/store/index.js
+++ src/store/index.js
@@ -20,8 +20,28 @@ const counterSlice = createSlice({
   },
 });
 
-const store = configureStore({
-  reducer: counterSlice.reducer,
+const authSlice = createSlice({
+  name: "authentication",
+  initialState: {
+    isAuthenticated: false,
+  },
+  reducers: {
+    login(state) {
+      state.isAuthenticated = true;
+    },
+    logout(state) {
+      state.isAuthenticated = false;
+    },
+  },
 });
 
+const store = configureStore({
+  reducer: {
+    counter: counterSlice.reducer,
+    auth: authSlice.reducer,
+  },
+});
+
+export const counterActions = counterSlice.actions;
+export const authActions = authSlice.actions;
 export default store;
